aa043a950edec3375bafe1ea54fb9e5b074399b5
Tim Ruszala <truszala@gmail.com>
2023-02-20 17:07:14 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,10 @@ module.exports = {
     title: `GatsbySite`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    // "gatsby-source-filesystem",
+    // "gatsby-transformer-sharp",
+  ],
+};
